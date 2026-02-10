fce97d13d57237b9f29586de5edc85c2f93c7b7e
Elaine Lyons <elaine@lyons.app>
2026-02-10 17:26:43 -0500

    Initial Commit

---
+++ Invocation/InvocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvocationApp.swift
+//  Invocation
+//
+//  Created by Elaine Lyons on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct InvocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
